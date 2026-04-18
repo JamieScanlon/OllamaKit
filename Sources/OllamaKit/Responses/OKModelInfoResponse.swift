3af3f131a415872a6b649d23475ab8a18d544dea
JamieScanlon <jsjphoto@gmail.com>
2026-04-17 17:51:22 -0700

    add image capability

--- Sources/OllamaKit/Responses/OKModelInfoResponse.swift
+++ Sources/OllamaKit/Responses/OKModelInfoResponse.swift
@@ -22,7 +22,7 @@ public struct OKModelInfoResponse: Decodable, Sendable {
     /// A string detailing the parameters or settings of the model.
     public let parameters: String
     
-    /// A list of model capabilities (like vision, tools, audio)
+    /// A list of model capabilities (like vision, tools, image, audio)
     public let capabilities: [Capability]
     
     /// An optional string containing system information about the model.
@@ -40,6 +40,7 @@ public struct OKModelInfoResponse: Decodable, Sendable {
         case vision
         case embedding
         case thinking
+        case image
         case audio
     }
     
